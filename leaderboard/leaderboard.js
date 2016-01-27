29ec080ef95fb8721f66c8571516e1019a00b89a
Garrick Ranck <granck@uno.edu>
2016-01-27 09:09:17 -0600

    Minor changes. Added functionality to clicking player and adding score.

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -7,13 +7,19 @@ if(Meteor.isClient){
 		'playerCount': function(){
 			return PlayerList.find().count()},
 		'selectedClass': function(){
-			return "selected"}
+			var playerId = this._id;
+			var selectedPlayer = Session.get('selectedPlayer');
+			if(playerId == selectedPlayer){
+				return "selected"}}
 	});//leaderboard helpers
 
 	Template.leaderboard.events({
 		'click .player': function(){
 			var playerId = this._id; 
-			Session.set('selectedPlayer', playerId);}
+			Session.set('selectedPlayer', playerId);},
+		'click .increment': function(){
+			var selectedPlayer = Session.get('selectedPlayer');
+			PlayerList.update(selectedPlayer, {$set: {score: 5}});}
 
 	});//leaderboard events
 
